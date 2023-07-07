ba78ae9d7b30b359d7bebcb4d64744b89122cebb
Bhumi <137621182+Bhumikatosu@users.noreply.github.com>
2023-07-07 20:51:30 +0530

    Update start.py
    Signed-off-by: Bhumi <137621182+Bhumikatosu@users.noreply.github.com>

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,21 +9,26 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🤭 𝙰𝙳𝙳  𝙼𝙴  𝙼𝙾𝙸  𝙱𝙰𝙱𝚈 😴",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                text="🤣 𝙳𝙴𝚅'𝚂 𝙶𝙵 🤭", url=f"https://t.me/Itz_darshaner"",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🍂 𝙷𝙴𝙻𝙿  𝙰𝙽𝙳  𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 🍂",
-                callback_data="settings_back_helper",
+                text="🌹 𝙳𝙴𝚅'𝚂  𝙱𝙷𝙰𝙸 🌹",
+                url=f"https://t.me/ITZ_NIKK_BABY",
+          
             ),
             InlineKeyboardButton(
                 text="🥀 𝚂𝙴𝚃𝚃𝙸𝙽𝙶 😂", callback_data="settings_helper"
             ),
+         
             InlineKeyboardButton(
-                text="🤣 𝙳𝙴𝚅'𝚂 𝙶𝙵 🤭", callback_data="https://t.me/Itz_darshaner"
-            
+                text="🔥 𝙳𝙴𝚅'𝚂  𝙵𝚁𝙸𝙴𝙽𝙳 💖", url=f"https://t.me/Mahakal_ki_mahakali"
+           
+            ),
+            InlineKeyboardButton(
+                text="🔥 𝙳𝙴𝚅'𝚂  𝙵𝚁𝙸𝙴𝙽𝙳 💖", url=f"https://t.me/ITZ_CANDY_SHARMA"
+           
             ),
         ],
      ]
