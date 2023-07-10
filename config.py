64b06940906cdd03e05a6f296b827e2ada5db443
Bhumi <137621182+Bhumikatosu@users.noreply.github.com>
2023-07-10 23:15:37 +0530

    Update config.py
    Signed-off-by: Bhumi <137621182+Bhumikatosu@users.noreply.github.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/d830a9dc5036865ebbfdb.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/dd69f43854ca9a3ec3c0e.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -89,7 +89,7 @@ GLOBAL_IMG_URL = "https://te.legra.ph/file/653a3e0097d66b31a8219.jpg"
 
 STATS_IMG_URL = "https://te.legra.ph/file/424dab41b3be5297d4360.jpg"
 
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/dd69f43854ca9a3ec3c0e.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/611a51fab25c07a4a7297.jpg"
 
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/dd69f43854ca9a3ec3c0e.jpg"
 
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/d830a9dc5036865ebbfdb.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/dd69f43854ca9a3ec3c0e.jpg"
